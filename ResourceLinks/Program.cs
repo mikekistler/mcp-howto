8734033dbe92b75b61cee8f4a4a046ef8cdb6892
Mike Kistler <mikekistler@microsoft.com>
2025-07-31 07:02:02 -0700

    Update SDK package version and misc package cleanup

--- ResourceLinks/Program.cs
+++ ResourceLinks/Program.cs
@@ -15,15 +15,7 @@ builder.Logging.AddConsole(options =>
     options.LogToStandardErrorThreshold = LogLevel.Information;
 });
 
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
